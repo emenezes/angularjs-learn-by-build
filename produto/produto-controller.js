da8b397a025ebf11158ff3f361b704c8d556ff64
emenezes <menezes.ime@gmail.com>
2018-04-19 12:33:51 -0300

    (19/04/2018) Melhorias na área de produto e criação da área de cliente renomeado criar.html (em produto) para index.html

--- produto/produto-controller.js
+++ produto/produto-controller.js
@@ -1,23 +1,28 @@
 (function(){
 
-angular.module("myStore",[]);
+angular.module('myStore',[]);
 
 })();
 
 (function(){
 
 function ProdutoController() {
-	this.listaProdutos = [];
+	let listaProdutosStored = localStorage.getItem('listaProdutos');
+	this.listaProdutos = (listaProdutosStored && JSON.parse(listaProdutosStored)) || [];
 	this.novoProduto = {};
-
+	this.estiloTabelaLinhaImpar = {'background-color':'blue'}
+	this.estiloTabelaLinhaPar = {'background-color':'red'}
+	var id = 0;
 	this.criarProduto = function() {
+		this.novoProduto.id = ++id;
 		this.listaProdutos.push(this.novoProduto);
 		this.novoProduto = {};
+		localStorage.setItem('listaProdutos',JSON.stringify(this.listaProdutos));
 	}
 }
 
 ProdutoController.$inject = [];
-angular.module("myStore")
-	.controller("ProdutoController", ProdutoController);
+angular.module('myStore')
+	.controller('ProdutoController', ProdutoController);
 
 })();
